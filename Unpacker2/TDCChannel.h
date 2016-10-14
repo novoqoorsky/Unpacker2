ae6a22d9d8b5eda04d7857c599b092bf2265c698
Aleksander Gajos <alek.gajos@gmail.com>
2016-10-14 13:32:23 +0200

    Optimize TDCChannel
    Use vectors in place of static arrays and remove
    obsolete methods. After these changes TDCChannel
    is in accordance with the present Go4-based Unpacker.

--- Unpacker2/TDCChannel.h
+++ Unpacker2/TDCChannel.h
@@ -5,24 +5,17 @@
 #include <TObject.h>
 #include <TClonesArray.h>
 #include <iostream>
-
-#define MAX_FULL_HITS 100
+#include <vector>
+using namespace std;
 
 class TDCChannel : public TObject {
 
 protected:
   Int_t channel;
-
-	double leadTime1;
-	double trailTime1;
-	double tot1;
-	double referenceDiff1;
-
-	double leadTimes[MAX_FULL_HITS];
-	double trailTimes[MAX_FULL_HITS];
-	double tots[MAX_FULL_HITS];
-	double referenceDiffs[MAX_FULL_HITS];
-	int hitsNum;
+  
+  vector<double> leadTimes;
+  vector<double> trailTimes;
+  int hitsNum;
 
   
 public:
@@ -31,20 +24,29 @@ public:
   ~TDCChannel();
   
   void SetChannel(Int_t channel) { this->channel = channel; }
-	int GetChannel() { return channel; }
+  int GetChannel() { return channel; }
+  int GetHitsNum() { return hitsNum; }
+  
+  void AddHit(double lead, double trail);
 
-	int GetHitsNum() { return hitsNum; }
+  double GetLeadTime(unsigned int mult) {
+    return leadTimes[mult];
+  }
 
-	void AddHit(double lead, double trail, double ref);
-	void AddHit(double lead, double trail);
-	double GetLeadTime1() { return leadTime1; }
-	double GetLeadTime(int mult) { return leadTimes[mult]; }
-	double GetTrailTime1() { return trailTime1; }
-	double GetTrailTime(int mult) { return trailTimes[mult]; }
-	double GetTOT1() { return tot1; }
-	int GetMult() { return hitsNum; }
-	double GetTOT(int mult) { return tots[mult]; }
+  double GetTrailTime(unsigned int mult) {
+    if( mult >= trailTimes.size() ){
+      std::cout<<"asked for trail time out of range."<<std::endl;
+      return 0.;
+    }
+    return trailTimes[mult];
+  }
 
+  int GetMult() { return hitsNum; }
+
+
+
+
+  
 
 
   ClassDef(TDCChannel,1);

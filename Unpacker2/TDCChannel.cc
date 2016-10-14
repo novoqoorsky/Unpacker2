cf47fe74b6a2493f12a76877592cf7240c99e4c9
Aleksander Gajos <alek.gajos@gmail.com>
2016-10-14 14:14:04 +0200

    Bug fix in TDCChannel

--- Unpacker2/TDCChannel.cc
+++ Unpacker2/TDCChannel.cc
@@ -13,12 +13,8 @@ TDCChannel::~TDCChannel() {}
 
 
 void TDCChannel::AddHit(double lead, double trail) {
-	if (hitsNum < MAX_FULL_HITS - 1) {
-	  
 	  leadTimes.push_back(lead);
 	  trailTimes.push_back(trail);
 	  
 	  hitsNum++;
-
-	}
 }

dc3344dd97ea08429a19b76255cfcff6e0bc371a
Aleksander Gajos <alek.gajos@gmail.com>
2017-02-11 05:01:47 +0100

    Fix memory in TClonesArray like in 51c53255

--- Unpacker2/Event.cc
+++ Unpacker2/Event.cc
@@ -8,7 +8,7 @@ ClassImp(Event);
 Event::Event() {   
   TDCHits = new TClonesArray("TDCHitExtended", 2100);
   
-  TDCHits->Clear();
+  TDCHits->Clear("C");
   
   totalNTDCHits = 0;
   
@@ -20,7 +20,7 @@ Event::Event() {
 TDCHit* Event::AddTDCHit(int channel) {
   //cerr<<"Event: adding TDC hit on channel "<<channel<<endl;
   TClonesArray& thits = *TDCHits;
-  TDCHit* hit = new (thits[totalNTDCHits++]) TDCHit();
+  TDCHit* hit = dynamic_cast<TDCHit*>(thits.ConstructedAt(totalNTDCHits++)); 
   hit->SetChannel(channel);
   return hit;
 }
@@ -28,14 +28,14 @@ TDCHit* Event::AddTDCHit(int channel) {
 TDCHitExtended* Event::AddTDCHitExtended(int channel) {
   //cerr<<"Event: adding TDC hit on channel "<<channel<<endl;
   TClonesArray& thits = *TDCHits;
-  TDCHitExtended* hit = new (thits[totalNTDCHits++]) TDCHitExtended();
+  TDCHitExtended* hit = dynamic_cast<TDCHitExtended*>(thits.ConstructedAt(totalNTDCHits++)); 
   hit->SetChannel(channel);
   return hit;
 }
 
 
 void Event::Clear(void) {  
-  TDCHits->Clear();
+  TDCHits->Clear("C");
   
   totalNTDCHits = 0;
   
